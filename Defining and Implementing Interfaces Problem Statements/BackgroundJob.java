877a184b70b9c91a6c4136688aac409e2202cf3a
muskanrawatt2012-dotcom <muskan.rawatt2012@gmail.com>
2025-11-03 01:42:00 +0530

    Delete Defining and Implementing Interfaces Problem Statements directory

--- Defining and Implementing Interfaces Problem Statements/BackgroundJob.java
+++
@@ -1,6 +0,0 @@
-public class BackgroundJob {
-    public static void main(String[] args) {
-        Runnable job = () -> System.out.println("Running background job...");
-        job.run(); 
-    }
-}
--- Defining and Implementing Interfaces Problem Statements/StringLengthChecker.java
+++
@@ -1,9 +0,0 @@
-import java.util.function.Function;
-
-public class StringLengthChecker {
-    public static void main(String[] args) {
-        Function<String, Integer> lengthCheck = str -> str.length();
-        String msg = "Hello Java Interfaces!";
-        System.out.println("Message length: " + lengthCheck.apply(msg));
-    }
-}
